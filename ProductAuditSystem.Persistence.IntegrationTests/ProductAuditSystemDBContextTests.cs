d97b32b44690c1f6973651700f5335254ba9c85f
JM Lugo <lugoc.jose@outlook.com>
2024-04-08 02:46:15 -0700

    Feat: Integration testing for ProductAuditSystemDBContext

---
+++ ProductAuditSystem.Persistence.IntegrationTests/ProductAuditSystemDBContextTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductAuditSystem.Domain;
+using ProductAuditSystem.Persistence.DataBaseContext;
+using Shouldly;
+
+namespace ProductAuditSystem.Persistence.IntegrationTests;
+
+public class ProductAuditSystemDBContextTests
+{
+    private readonly ProductAuditSystemDBContext _productAuditSystemDBContext;
+
+    public ProductAuditSystemDBContextTests()
+    {
+        var dbOptions = new DbContextOptionsBuilder<ProductAuditSystemDBContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+
+        _productAuditSystemDBContext = new ProductAuditSystemDBContext(dbOptions);
+    }
+
+    [Fact]
+    public async Task Save_DateCreatedAtValueAsync()
+    {
+        var auditStatus = new AuditStatus
+        {
+            Id = 1,
+            Status = "In-Process"
+        };
+
+        await _productAuditSystemDBContext.AuditStatus.AddAsync(auditStatus);
+        await _productAuditSystemDBContext.SaveChangesAsync();
+
+        auditStatus.FechaCreacion.ShouldNotBeNull();
+    }
+
+    [Fact]
+    public async void Save_DateModifiedAtValue()
+    {
+        var auditStatus = new AuditStatus
+        {
+            Id = 1,
+            Status = "In-Process"
+        };
+
+        await _productAuditSystemDBContext.AuditStatus.AddAsync(auditStatus);
+        await _productAuditSystemDBContext.SaveChangesAsync();
+
+        auditStatus.FechaModificacion.ShouldNotBeNull();
+    }
+}
