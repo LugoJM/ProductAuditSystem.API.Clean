1647f7819e0914cd5f90248df36de9aa79751920
JM Lugo <lugoc.jose@outlook.com>
2024-04-08 02:45:07 -0700

    Feat: Unit testing for all AuditStatus queries

---
+++ ProductAuditSystem.Application.UnitTests/Features/AuditStatus/Queries/GetAuditStatusQueryHandlerTest.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using Moq;
+using ProductAuditSystem.Application.Contracts.Persistence;
+using ProductAuditSystem.Application.Features.AuditStatus.Queries.GetAuditStatus;
+using ProductAuditSystem.Application.MappingProfiles;
+using ProductAuditSystem.Application.UnitTests.Mocks;
+using Shouldly;
+
+namespace ProductAuditSystem.Application.UnitTests.Features.AuditStatus.Queries;
+
+public class GetAuditStatusQueryHandlerTest
+{
+    private readonly Mock<IAuditStatusRepository> _mock;
+    private readonly IMapper _mapper;
+    public GetAuditStatusQueryHandlerTest()
+    {
+        _mock = MockAuditStatusRepository.GetMockAuditStatusRepository();
+
+        var mapperConfig = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<AuditStatusProfile>();
+        });
+
+        _mapper = mapperConfig.CreateMapper();
+    }
+
+    [Fact]
+    public async Task GetAuditByIdTest()
+    {
+        const int AuditStatusID = 1;
+
+        var handler = new GetAuditStatusQueryHandler(_mapper, _mock.Object);
+
+        var result = await handler.Handle(new GetAuditStatusQuery(AuditStatusID), CancellationToken.None);
+
+        result.ShouldNotBeNull();
+    }
+}
---
+++ ProductAuditSystem.Application.UnitTests/Features/AuditStatus/Queries/GetAuditsStatusQueryHandlerTests.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using Moq;
+using ProductAuditSystem.Application.Common.SharedDTOs;
+using ProductAuditSystem.Application.Contracts.Persistence;
+using ProductAuditSystem.Application.Features.AuditStatus.Queries.GetAuditsStatus;
+using ProductAuditSystem.Application.MappingProfiles;
+using ProductAuditSystem.Application.UnitTests.Mocks;
+using Shouldly;
+
+namespace ProductAuditSystem.Application.UnitTests.Features.AuditStatus.Queries;
+
+public class GetAuditStatusQueryHandlerTests
+{
+    private readonly Mock<IAuditStatusRepository> _mockRepo;
+    private readonly IMapper _mapper;
+
+    public GetAuditStatusQueryHandlerTests()
+    {
+        _mockRepo = MockAuditStatusRepository.GetMockAuditStatusRepository();
+
+        var mapperConfig = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<AuditStatusProfile>();
+        });
+
+        _mapper = mapperConfig.CreateMapper();
+    }
+
+    [Fact]
+    public async Task GetAuditStatusListTest()
+    {
+        var handler = new GetAuditsStatusQueryHandler(_mapper, _mockRepo.Object);
+
+        var result = await handler.Handle(new GetAuditsStatusQuery(), CancellationToken.None);
+
+        result.ShouldBeOfType<List<AuditStatusDTO>>();
+        result.Count.ShouldBe(3);
+    }
+}
